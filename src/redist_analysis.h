323a042aabcc918b64a0b9879f394fc031d2d502
Ben Fifield <bfifield@princeton.edu>
2015-05-15 00:00:00 +0000

    version 1.0

---
+++ src/redist_analysis.h
@@ -0,0 +1,8 @@
+#ifndef redist_analysis_h
+#define redist_analysis_h
+
+Rcpp::NumericVector segregationcalc(Rcpp::NumericMatrix distmat,
+				    Rcpp::NumericVector grouppop,
+				    Rcpp::NumericVector fullpop);
+#endif
+
